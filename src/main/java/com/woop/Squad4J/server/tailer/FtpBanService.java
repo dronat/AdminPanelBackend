9fe89c2ff767226ae500234c5f02910a46165504
Andrew <a.garasimchuk@propellerads.net>
2022-10-08 02:36:34 +0300

    changed delay of bans

--- src/main/java/com/woop/Squad4J/server/tailer/FtpBanService.java
+++ src/main/java/com/woop/Squad4J/server/tailer/FtpBanService.java
@@ -25,7 +25,7 @@ public class FtpBanService implements Runnable{
     private final String ABSOLUTE_FILE_PATH = ConfigLoader.get("server.banAbsolutePath", String.class);
     private final String ENCODING = "UTF-8";
     private final String FILE_NAME = "Bans_test.cfg";
-    private final long DELAY_IN_MILLIS = 30 * 60000;
+    private final long DELAY_IN_MILLIS = 5 * 60000;
     private volatile boolean run;
 
     @Override
