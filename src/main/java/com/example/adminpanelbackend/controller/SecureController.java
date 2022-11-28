45873efeb690310a8a42ff234ef3907e30c372e4
a.garasimchuk <a.garasimchuk@propellerads.net>
2022-11-28 20:20:10 +0100

    fix permanent bans

--- src/main/java/com/example/adminpanelbackend/controller/SecureController.java
+++ src/main/java/com/example/adminpanelbackend/controller/SecureController.java
@@ -129,7 +129,7 @@ public class SecureController {
                     put("numOfActiveBans", player
                             .getPlayersBansBySteamId()
                             .stream()
-                            .filter(ban -> ban.getExpirationTime() != null && ban.getExpirationTime().after(new Date()) && !ban.getIsUnbannedManually())
+                            .filter(ban -> ban.getExpirationTime() == null || ban.getExpirationTime().after(new Date()) && !ban.getIsUnbannedManually())
                             .count()
                     );
                 }}
@@ -187,7 +187,7 @@ public class SecureController {
             put("numOfActiveBans", player
                     .getPlayersBansBySteamId()
                     .stream()
-                    .filter(ban -> ban.getExpirationTime() != null && ban.getExpirationTime().after(new Date()) && !ban.getIsUnbannedManually())
+                    .filter(ban -> ban.getExpirationTime() == null || ban.getExpirationTime().after(new Date()) && !ban.getIsUnbannedManually())
                     .count()
             );
         }};
