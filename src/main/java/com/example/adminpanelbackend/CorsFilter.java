c8b8a0e8daf6bc5b0fafba460e7fff0b1722bd25
Andrew <a.garasimchuk@propellerads.net>
2023-03-26 18:51:25 +0200

    some changes

--- src/main/java/com/example/adminpanelbackend/CorsFilter.java
+++ src/main/java/com/example/adminpanelbackend/CorsFilter.java
@@ -25,8 +25,9 @@ public class CorsFilter implements Filter {
 
             // Access-Control-Allow-Origin
             String origin = request.getHeader("Origin");
-//            response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
-            response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+            //response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
+            response.addHeader("Access-Control-Allow-Origin", "http://185.31.160.131:3000");
+            //response.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
             response.setHeader("Vary", "Origin");
 
             // Access-Control-Max-Age
